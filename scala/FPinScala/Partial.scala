0280c5dd16486544cb266214efe89f3623f42a59
vinay631 <i.m.vinaym@gmail.com>
2015-06-22 16:09:20 -0400

    FP in Scala - Solution to exercises related to currying, uncurrying and composition

---
+++ scala/FPinScala/Partial.scala
@@ -0,0 +1,10 @@
+object Partial extends App {
+    def curry[A, B, C](f: (A, B) => C): A => (B => C) =
+        (a: A) => (b:B) => f(a, b)
+
+    def uncurry[A, B, C](f: A => B => C): (A, B) => C = 
+        (a:A, b:B) => f(a)(b)
+
+    def compose[A, B, C](f: B => C, g: A => B): A => C =
+        (a:A) => f(g(a))
+}
