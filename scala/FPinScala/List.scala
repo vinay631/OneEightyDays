ed0ade13efe2d43e5102376ec2d07b92f0f49df2
vinay631 <i.m.vinaym@gmail.com>
2015-06-23 17:32:51 -0400

    Solution for 3.2

---
+++ scala/FPinScala/List.scala
@@ -0,0 +1,17 @@
+sealed trait List[+A]
+
+case object Nil extends List[Nothing]
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+object List {
+
+    def apply[A](as: A*): List[A] = 
+        if (as.isEmpty) Nil
+        else Cons(as.head, apply(as.tail: _*))
+
+    #FP Exercise 3.2
+    def tail[A](as: List[A]): List[A] = as match {
+        case Nil => Nil
+        case Cons(x, xs) => xs
+    }
+}
