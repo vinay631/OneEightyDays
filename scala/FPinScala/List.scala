b80eb53e239d10002095f76a4c5fdaecaf2cd9f9
vinay631 <i.m.vinaym@gmail.com>
2015-06-25 15:56:15 -0400

    Implemented dropWhile

--- scala/FPinScala/List.scala
+++ scala/FPinScala/List.scala
@@ -26,4 +26,10 @@ object List {
         case n if n < 0 => l
         case _ => drop(tail(l), n-1)
     }
+
+    //FP Exercise 3.5
+    def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+        case Cons(x, xs) if f(x) => dropWhile(xs, f)
+        case _ => l
+    }
 }
