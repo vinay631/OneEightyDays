be30ad69a80bfa355fa46512ea80fa9bfa4da805
vinay631 <i.m.vinaym@gmail.com>
2015-07-12 21:11:59 -0400

    Tree datastructure - size method

---
+++ scala/FPinScala/Tree.scala
@@ -0,0 +1,10 @@
+sealed trait Tree[+A]
+case class Leaf[A](value: A) extends Tree[A]
+case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+object Tree{
+    def size[A](tree:Tree[A]):Int = tree match {
+        case Leaf(_) => 1
+        case Branch(l, r) => 1 + size(l) + size(r)
+    }
+}
