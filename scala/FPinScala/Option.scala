d30d05cd89031e4230038d04a802fc4af32dbf4b
vinay631 <i.m.vinaym@gmail.com>
2015-09-15 14:06:43 -0400

    Sequence implemented

--- scala/FPinScala/Option.scala
+++ scala/FPinScala/Option.scala
@@ -45,4 +45,9 @@ object Option {
     def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = {
         a flatMap (aa => b map (bb => f(aa, bb)))
     }
+
+    def sequence[A](a: List[Option[A]]): Option[List[A]] = a match {
+       case Nil => Some(Nil) 
+       case h :: t => h flatMap(hh => sequence(t) map (hh :: _))
+    }
 }
